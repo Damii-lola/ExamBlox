5f35a6efab638996cf8cd60cdca9c395c72abecc
Damiii <78701527+Damii-lola@users.noreply.github.com>
2025-09-28 18:48:55 +0100

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -9,7 +9,7 @@ const PROTECTED_ADMIN = {
   username: 'damii-lola',
   name: 'Damii Lola',
   email: 'damii.lola.admin@examblox.com',
-  password: 'admin123secure',
+  password: 'God_G1ve_M3_P0wer',
   plan: 'premium',
   role: 'admin',
   isPermanent: true,
